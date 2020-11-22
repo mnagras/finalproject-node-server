1558175055298a5f048092c78a8035591a2c587a
Manami Nagras <nagras.m@husky.neu.edu>
2020-11-22 23:23:37 +0900

    updated

---
+++ routes/productAPI.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+const unirest = require("unirest");
+/* request.query({ "entry": req.params.word });*/
+/* GET users listing. */
+router.get('/:productId', function(req, res, next) {
+  /* res.send('API is working properly'); */
+  const request = unirest("GET",
+  "https://manamionlinestore.myshopify.com/admin/api/2020-10/products/" + req.params.productId + ".json");
+
+  request.headers({
+    "Authorization": 'Basic ' +
+    Buffer.from("865f308f8c39f7a629e18ed20538c4e9" + ':' +
+    "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").toString('base64'),
+  });
+
+  request.end(function (response) {
+    if (response.error) throw new Error(response.error);
+     console.log(response);
+     res.send(response);
+    /*res.json(response.body.associations_scored || {});*/
+  });
+
+});
+
+module.exports = router;
