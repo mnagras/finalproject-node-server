978c9e59210a1a2242427e3ac9e06148124df718
Manami Nagras <nagras.m@husky.neu.edu>
2020-12-10 23:20:23 +0900

    updated

--- routes/users.js
+++ routes/users.js
@@ -5,21 +5,54 @@ const userUrl = "http://localhost:8080/users"
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+ const request = unirest("GET", userUrl)
+  .header("Content-Type", "application/json");
+
+  console.log("before getting");
+    request.end(function (response) {
+      if (response.error) throw new Error(response.error);
+       console.log("after getting");
+       res.send(response);
+       console.log(response.body);
+});
 });
 
-router.post('/users', function(req, res, next) {
- const request = unirest("POST", userUrl);
+router.post('/', function(req, res, next) {
+console.log("I am here for user posting");
+ const request = unirest("POST", userUrl)
+ .header("Content-Type", "application/json")
+ .send(req.body);
 
- request.body(req.params.user).asJson();
+//console.log(req.body);
 
+ console.log("before posting");
    request.end(function (response) {
      if (response.error) throw new Error(response.error);
-      console.log(response);
+      console.log("after posting");
       res.send(response);
+      console.log(response.body);
      /*res.json(response.body.associations_scored || {});*/
    });
-
 });
+
+router.post('/login', function(req, res, next) {
+console.log("I am here for login posting");
+ const request = unirest("POST", userUrl + "/login")
+ .header("Content-Type", "application/json")
+ .send(req.body);
+
+//console.log(req.body);
+
+ console.log("before login");
+   request.end(function (response) {
+     //if (response.error) throw new Error(response.error);
+      console.log("after login");
+      res.send(response);
+      console.log(response.body);
+     /*res.json(response.body.associations_scored || {});*/
+   });
+});
+
+
 
 module.exports = router;
